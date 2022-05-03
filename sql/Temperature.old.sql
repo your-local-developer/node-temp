cc9569e812e636087f6e711be05db1dcd43b5851
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2022-05-03 10:21:19 +0200

    update databse

--- sql/Temperature.old.sql
+++ sql/Temperature.old.sql
@@ -4,5 +4,5 @@ CREATE TABLE Temperature (
   temperature_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   temperature INT NOT NULL,
   humidity INT NOT NULL,
-  time_of_collection TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+  day_of_collection DATE NOT NULL
  );
