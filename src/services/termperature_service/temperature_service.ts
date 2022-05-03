27ee2ecb4f4328bc98337a0c7ae771792352ddef
your-local-developer <73035294+your-local-developer@users.noreply.github.com>
2022-05-03 10:21:56 +0200

    Logging in service

--- src/services/termperature_service/temperature_service.ts
+++ src/services/termperature_service/temperature_service.ts
@@ -1,24 +1,29 @@
-import type { Pool } from "mariadb";
+import type { Pool, PoolConnection } from "mariadb";
 import moment from "moment";
 import { pool } from "../../db/db.js"
 import { readDhtSensor } from "../../lib/temperature_reader.mjs"
 
 async function insertTemperature(pool: Pool) {
-    let conn;
+    let conn: PoolConnection | undefined;
     try {
         const { humidity, temperature } = await readDhtSensor(11, 4)
-        const timestamp = moment(Date.now()).format('YYYY-MM-DD HH-mm-ss')
+        console.log(humidity, temperature)
+        const timestamp = moment().format('YYYY-MM-DD HH-mm-ss')
+        console.log(`Timestamp: ${timestamp}`)
         conn = await pool.getConnection();
+        console.log(`Connection is is valid: ${conn?.isValid()}`)
         const res = await conn.query(
-            /*sql*/`INSERT INTO Temperature(temperature,humidity,day_of_collection) VALUE(?,?,?)`,
-            [temperature, humidity, timestamp]
+            /*sql*/`INSERT INTO Temperature(temperature,humidity) VALUE(?,?)`,
+            [temperature, humidity]
         );
+
         console.log(res)
 
     } catch (err) {
-        throw err;
+        console.error(err)
     } finally {
-        if (conn) return conn.end();
+        if (conn) conn.end();
+        console.log(`Connection is is valid: ${conn?.isValid()}`)
     }
 }
 
